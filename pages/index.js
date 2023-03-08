1f5325518eefadab0b13e603486cadb52c1662ee
MaorShtern <93667425+MaorShtern@users.noreply.github.com>
2023-03-08 23:44:13 +0200

    08.03

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>דף הבית - אלי שטרן פרוייקטים</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
+      </div>
+  )
+}
